b72e0546b56e6d648c9abf3bb228e357a183d3f9
Victor Jose Tejero Maella <vjtejerom@gmail.com>
2025-03-17 23:01:25 +0100

    Initial commit: Configuración inicial del proyecto

---
+++ viajando-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'viajando-app',
+  webDir: 'www'
+};
+
+export default config;
